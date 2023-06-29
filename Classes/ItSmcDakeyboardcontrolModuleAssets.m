9139fa54e87c149733e90a42b40248d776d77618
Richard Lustemberg <richard@theappartisan.com>
2023-06-29 13:19:06 +0200

    updated build

--- Classes/ItSmcDakeyboardcontrolModuleAssets.m
+++ Classes/ItSmcDakeyboardcontrolModuleAssets.m
@@ -1,40 +1,24 @@
 /**
- * Copyright (c) 2014 SMC Treviso s.r.l. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * Appcelerator Titanium is Copyright (c) 2009-2010 by Appcelerator, Inc.
- * and licensed under the Apache Public License (version 2)
+ * This is a generated file. Do not edit or your changes will be lost
  */
-
 #import "ItSmcDakeyboardcontrolModuleAssets.h"
 
 extern NSData* filterDataInRange(NSData* thedata, NSRange range);
 
 @implementation ItSmcDakeyboardcontrolModuleAssets
 
-- (NSData*) moduleAsset
+- (NSData *)moduleAsset
 {
-	//##TI_AUTOGEN_BEGIN asset
-	//Compiler generates code for asset here
-	return nil; // DEFAULT BEHAVIOR
-	//##TI_AUTOGEN_END asset
+  
+
+  return nil;
 }
 
-- (NSData*) resolveModuleAsset:(NSString*)path
+- (NSData *)resolveModuleAsset:(NSString *)path
 {
-	//##TI_AUTOGEN_BEGIN resolve_asset
-	//Compiler generates code for asset resolution here
-	return nil; // DEFAULT BEHAVIOR
-	//##TI_AUTOGEN_END resolve_asset
+  
+
+  return nil;
 }
 
 @end
